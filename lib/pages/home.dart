a213b1f0b78fa82bc172e1bea5d20e92971dcd68
HendAdel <hend.adel.m@gmail.com>
2024-07-16 22:50:14 +0300

    Add slider, use seek method. end of day1.

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -21,6 +21,7 @@ class _HomePageState extends State<HomePage> {
   void initPlayer() async {
     await assetsAudioPlayer.open(
         autoStart: false,
+        loopMode: LoopMode.playlist,
         (Playlist(audios: [
           Audio(
             "assets/1.mp3",
@@ -60,48 +61,99 @@ class _HomePageState extends State<HomePage> {
                   color: Colors.lightBlue,
                 ),
                 child: Center(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      const Text(
-                        'Song Name',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      const SizedBox(
-                        height: 25,
-                      ),
-                      getBtnWIdGet,
-                      const SizedBox(
-                        height: 10,
-                      ),
-                      const Text(
-                        '00:00 / 2:30',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 17,
-                          fontWeight: FontWeight.w600,
-                        ),
-                      ),
-                    ],
-                  ),
+                  child: StreamBuilder(
+                      stream: assetsAudioPlayer.realtimePlayingInfos,
+                      builder: (context, snapShots) {
+                        if (snapShots.connectionState ==
+                            ConnectionState.waiting) {
+                          return const Center(
+                            child: CircularProgressIndicator(),
+                          );
+                        }
+                        return Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text(
+                                assetsAudioPlayer.getCurrentAudioTitle == ''
+                                    ? 'Please play your song!'
+                                    : assetsAudioPlayer.getCurrentAudioTitle,
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              const SizedBox(
+                                height: 25,
+                              ),
+                              Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  IconButton(
+                                      onPressed:
+                                          snapShots.data?.current?.index == 0
+                                              ? null
+                                              : () {
+                                                  assetsAudioPlayer.previous();
+                                                },
+                                      icon: Icon(Icons.skip_previous)),
+                                  getBtnWIdGet,
+                                  IconButton(
+                                      onPressed:
+                                          snapShots.data?.current?.index ==
+                                                  (assetsAudioPlayer.playlist
+                                                              ?.audios.length ??
+                                                          0) -
+                                                      1
+                                              ? null
+                                              : () {
+                                                  assetsAudioPlayer.next();
+                                                },
+                                      icon: Icon(Icons.skip_next)),
+                                ],
+                              ),
+                              const SizedBox(
+                                height: 10,
+                              ),
+                              Slider(
+                                  value: snapShots
+                                          .data?.currentPosition.inSeconds
+                                          .toDouble() ??
+                                      0.0,
+                                  min: 0,
+                                  max: snapShots.data?.duration.inSeconds
+                                          .toDouble() ??
+                                      0.0,
+                                  onChanged: (value) {
+                                    assetsAudioPlayer
+                                        .seek(Duration(seconds: 1));
+                                  }),
+                              Text(
+                                '${secondsToMinutes(snapShots.data?.currentPosition.inSeconds ?? 0)} / ${secondsToMinutes(snapShots.data?.duration.inSeconds ?? 0)}',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 17,
+                                  fontWeight: FontWeight.w600,
+                                ),
+                              ),
+                            ],
+                          ),
+                        );
+                      }),
                 ),
               ),
-              // IconButton(icon: assetsAudioPlayer.builderIsPlaying(
-              //     builder: (context, isPlaying) {
-              //   return Icon(isPlaying ? Icons.pause : Icons.play_arrow);
-              // }), onPressed: () async {
-              //   await assetsAudioPlayer.open(
-              //     Audio("assets/1.mp3"),
-              //   );
-              //   setState(() {});
-              // }),
             ],
           ),
         ));
+  }
+
+  String secondsToMinutes(int seconds) {
+    String minutes = (seconds ~/ 60).toString();
+    String secondsString = (seconds % 60).toString();
+    return '${minutes.padLeft(2, '0')}:${secondsString.padLeft(2, '0')}';
   }
 
   Widget get getBtnWIdGet =>
